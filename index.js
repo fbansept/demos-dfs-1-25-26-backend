f12b5394b660101ec8380293abe83ddadaa3dc35
fbansept <bansept.franck@gmail.com>
2026-01-28 14:25:46 +0100

    ajout crud

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const categories = [
       "https://kissmychef.com/wp-content/uploads/2020/07/cornetteria.jpg",
     ],
   },
-  { id: 2, titre: "B", images: [] },
+  { id: 2, titre: "B2", images: [] },
   {
     id: 3,
     titre: "C",
@@ -37,18 +37,84 @@ app.get("/categories", (req, res) => {
 app.post("/image", (req, res) => {
   const { url } = req.body;
 
+  if (!url) {
+    return res
+      .status(400)
+      .json({ message: "L'URL de l'image est obligatoire" });
+  }
+
+  try {
+    const urlImage = new URL(url);
+
+    if (urlImage.protocol !== "http:" && urlImage.protocol !== "https:") {
+      return res
+        .status(400)
+        .json({ message: "L'URL de l'image doit être en http / https" });
+    }
+  } catch {
+    return res.status(400).json({ message: "L'URL de l'image est mal formée" });
+  }
+
   //a remplacer par une requete insert dans une BDD
   categories[0].images.push(url);
 
-  res.json({ message: "Image ajoutée avec succès" });
+  res.status(201).json({ message: "Image ajoutée avec succès" });
 });
 
-//
+//deplacement d'une image entre catégories
 app.patch("/image/:idCategorie", (req, res) => {
   const idCategorie = parseInt(req.params.idCategorie);
 
-  //ajouter le déplacement de l'image
+  const { indexImage, monter } = req.body;
 
+  if (monter == null || indexImage == null) {
+    return res.status(400).json({ message: "Informations obligatoires" });
+  }
+
+  const indexCategorieOrigine = categories.findIndex(
+    (cat) => cat.id === idCategorie,
+  );
+
+  if (indexCategorieOrigine === -1) {
+    return res.status(404).json({ message: "Catégorie non trouvée" });
+  }
+
+  const categorie = categories[indexCategorieOrigine];
+
+  console.log(idCategorie, categories);
+
+  const urlImageAdeplacer = categorie.images[indexImage];
+
+  const indexCategorieDestination = indexCategorieOrigine + (monter ? -1 : 1);
+
+  //on copie l'image dans la categorie du dessous/dessus
+  categories[indexCategorieDestination].images.push(urlImageAdeplacer);
+
+  //on supprime l'image de la categorie actuelle
+  categorie.images.splice(indexImage, 1);
+
+  res.json({ message: "Image déplacée avec succès" });
+});
+
+app.delete("/image/:idCategorie", (req, res) => {
+  const idCategorie = parseInt(req.params.idCategorie);
+
+  const { indexImage } = req.body;
+
+  if (indexImage == null) {
+    return res.status(400).json({ message: "Informations obligatoires" });
+  }
+
+  const categorie = categories.find((cat) => cat.id === idCategorie);
+
+  if (!categorie) {
+    return res.status(404).json({ message: "Catégorie non trouvée" });
+  }
+
+  //on supprime l'image de la categorie actuelle
+  categorie.images.splice(indexImage, 1);
+
+  res.json({ message: "Image supprimée avec succès" });
 });
 
 app.listen(port, () => {
